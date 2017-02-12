54391ef2fdcb2fe2f15f89200101022ea0d7d6c2
Lansana Camara <lansanacamara@github.com>
2017-02-12 14:16:11 -0500

    Order by descending order

--- app/Http/Controllers/ListController.php
+++ app/Http/Controllers/ListController.php
@@ -13,7 +13,7 @@ class ListController extends Controller
 
     public function show()
     {
-        $newcomers = Newcomer::paginate(3);
+        $newcomers = Newcomer::orderBy('created_at', 'desc')->paginate(3);
 
         return view('list', [
             'newcomers' => $newcomers,
